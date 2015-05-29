1900ace734d67ae7417a22cd8e787c2b1608c242
Yu KATO <y.katsew@gmail.com>
2015-05-30 00:19:57 +0900

    change lab to kusaka san's model
    change wall and floor

--- Assets/Scripts/ActionCreator.cs
+++ Assets/Scripts/ActionCreator.cs
@@ -5,61 +5,25 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
+
 public class ActionCreator : MonoBehaviour {
+	
+	public GameObject floorObj;
+	public GameObject wallObj;
 
-	[SerializeField]
-	public GameObject FloorGO;
-	public List<GameObject> WallGOs;
+	public List<Texture> floorTextureList;
+	public List<Texture> wallTextureList;
 
-	[SerializeField]
-	public List<Material> floorMaterialList;
-
-	[SerializeField]
-	public List<Material> wallMaterialList;
-
-	public enum TEXTURE_TYPE {
-		TEXTURE_DEFAULT,
-		TEXTURE_WALL,
-		TEXTURE_CHAIR
-	};
-
-	[SerializeField]
 	public void ChangeFloorTextureTo (int textureType) {
 
-		
-		FloorGO.renderer.material = floorMaterialList [textureType];			
-
-//		switch (textureType) {
-//			case TEXTURE_TYPE.TEXTURE_WALL:
-//				FloorGO.renderer.material = floorMaterialList [(int) TEXTURE_TYPE.TEXTURE_WALL];
-//				break;
-//			case TEXTURE_TYPE.TEXTURE_CHAIR:
-//				FloorGO.renderer.material = floorMaterialList [(int) TEXTURE_TYPE.TEXTURE_CHAIR];
-//				break;
-//			case
-//			TEXTURE_TYPE.TEXTURE_DEFAULT:
-//			default:
-//				FloorGO.renderer.material = floorMaterialList [(int) TEXTURE_TYPE.TEXTURE_DEFAULT];
-//				break;
-//		}
-
+		floorObj.renderer.material.mainTexture = floorTextureList [textureType];
 
 	}
 
 	public void ChangeWallTextureTo (int textureType) {
 
-		WallGOs.ForEach (
-			(GameObject obj) => obj.renderer.material = wallMaterialList [textureType]
-		);			
-		
+		wallObj.renderer.material.mainTexture = wallTextureList [textureType];
 
 	}
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -7,10 +7,10 @@ using System.Collections;
 public class MenuController : MonoBehaviour {
 
 	public void ShowMenu() {
-		this.gameObject.transform.localPosition = new Vector3 (0.0f, 0.0f, 0.0f);
+		this.gameObject.SetActive (true);
 		
 	}
 	public void CloseMenu() {
-		this.gameObject.transform.localPosition = new Vector3 (1136.0f, 0.0f, 0.0f);
+		this.gameObject.SetActive (false);
 	}
 }
